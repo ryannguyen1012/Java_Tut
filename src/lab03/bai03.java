7a9f769a6c3bc2b277c659860a2656c2a862f4e8
Tap Lam Game With Ryan <ryannguyen011201@gmail.com>
2025-02-09 01:09:58 +0700

    update 09/02/2025

--- src/lab03/bai03.java
+++ src/lab03/bai03.java
@@ -1,8 +1,33 @@
 package lab03;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class bai03 {
 	public static void main(String[] args) {
 		System.out.println("run bai 03");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Nhap so luong phan tu cua array: ");
+		int size = scanner.nextInt();
+		int[] a = new int[size];
+		System.out.println("Nhap phan tu:");
+		for (int i = 0; i < size; i++) {
+			a[i] = scanner.nextInt();
+		}
+		Arrays.sort(a);
+//		System.out.println(Arrays.toString(a));
+//		System.out.println("max= " + a[a.length - 1]);
+//		System.out.println("min= " + a[0]);
+		int max = a[0];
+		for (int i = 0; i < a.length; i++) {
+			max = Math.max(max, a[i]);
+		}
+		int min = a[0];
+		for (int i = 0; i < a.length; i++) {
+			min = Math.min(min, a[i]);
+		}
 
+		System.out.println("max: " + max);
+		System.out.println("min: " + min);
 	}
 }
